7d058ae1515d8c8e805d6ba2235f383114d9e58c
Jitendra Sharma <jitendra149.tech@gmail.com>
2022-03-03 17:49:30 +0530

    Developed Till 20 Data Layred

--- src/Services/Catalog/Catalog.API/Data/IcatalogContext.cs
+++ src/Services/Catalog/Catalog.API/Data/IcatalogContext.cs
@@ -1,13 +1,9 @@
 ﻿using Catalog.API.Entities;
 using MongoDB.Driver;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Catalog.API.Data
 {
-    public interface IcatalogContext
+    public interface ICatalogContext
     {
         IMongoCollection<Product> Products {get;}
     }
