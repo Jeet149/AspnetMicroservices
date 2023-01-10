ca5b4bc124dba98a3f97e931f33d7d49377791d1
Jitendra Sharma <jitendra149.tech@gmail.com>
2023-01-10 17:58:37 +0530

    Basket Microservices Developed and tested on Local.

---
+++ src/Services/Basket/Baskt.API/Repositories/BasketReposittory.cs
@@ -0,0 +1,39 @@
+﻿using Baskt.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Baskt.API.Repositories
+{
+    public class BasketReposittory : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketReposittory(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+
+            if (String.IsNullOrEmpty(basket))
+                return null;
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _redisCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            return await GetBasket(basket.UserName);
+        }
+        public async Task Deletebasket(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+    }
+}
